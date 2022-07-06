ff3a5766c335578dc0585ac2090e8282d1474709
Daniel Haarhoff <daniel@rknt.de>
2022-07-06 12:03:50 +0100

    only display reward after click

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,12 @@ import { pipe } from 'fp-ts/function';
 import * as O from 'fp-ts/Option';
 import { sequenceS } from 'fp-ts/lib/Apply';
 
+const displayReward = (result: HTMLElement) => () => {
+  result.innerHTML = `
+    <img src="https://www.akc.org/wp-content/uploads/2018/03/Two-Newfoundland-puppies-running-outside-header.jpg" alt="">
+  `
+}
+
 pipe(
   {
     prompt: O.fromNullable(document.getElementById('prompt')),
@@ -12,6 +18,8 @@ pipe(
   sequenceS(O.Apply),
   O.match(
     () => {},
-    () => {},
+    ({left, result}) => {
+      left.addEventListener('click', displayReward(result))
+    },
   ),
 );
