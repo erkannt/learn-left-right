c654310a3f93c98b38eace45e4ae42df1f773ad6
Daniel Haarhoff <daniel@rknt.de>
2022-07-06 12:14:50 +0100

    Introduce requestAnimationFrame based main loop

--- src/index.ts
+++ src/index.ts
@@ -2,24 +2,30 @@ import { pipe } from 'fp-ts/function';
 import * as O from 'fp-ts/Option';
 import { sequenceS } from 'fp-ts/lib/Apply';
 
+let prompt;
+let left;
+let right;
+let result;
+
+const init = () => {
+  prompt = document.getElementById('prompt') as HTMLElement;
+  left = document.getElementById('left') as HTMLElement;
+  right = document.getElementById('right') as HTMLElement;
+  result = document.getElementById('result') as HTMLElement;
+
+  left.addEventListener('click', displayReward(result));
+
+  window.requestAnimationFrame(mainLoop);
+};
+
+const mainLoop = () => {
+  window.requestAnimationFrame(mainLoop);
+};
+
 const displayReward = (result: HTMLElement) => () => {
   result.innerHTML = `
     <img src="https://www.akc.org/wp-content/uploads/2018/03/Two-Newfoundland-puppies-running-outside-header.jpg" alt="">
-  `
-}
+  `;
+};
 
-pipe(
-  {
-    prompt: O.fromNullable(document.getElementById('prompt')),
-    left: O.fromNullable(document.getElementById('left')),
-    right: O.fromNullable(document.getElementById('right')),
-    result: O.fromNullable(document.getElementById('result')),
-  },
-  sequenceS(O.Apply),
-  O.match(
-    () => {},
-    ({left, result}) => {
-      left.addEventListener('click', displayReward(result))
-    },
-  ),
-);
+window.onload = init;
